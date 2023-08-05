b942073b160e7b1ce55d5e39782d7babd0ea3d93
Kamil D <kamilducz1989@gmail.com>
2023-08-05 10:02:32 +0200

    Fix names in contact seeder

---
+++ backend/ContactList.Infrastructure/Migrations/20230805080157_Fix names in contact seeder.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContactList.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Fixnamesincontactseeder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Contacts",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "Email", "FirstName", "LastName" },
+                values: new object[] { "eg@gmail.com", "Edgar", "Gold" });
+
+            migrationBuilder.UpdateData(
+                table: "Contacts",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "Email", "FirstName", "LastName" },
+                values: new object[] { "rsapphire@gmail.com", "Roger", "Sapphire" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Contacts",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "Email", "FirstName", "LastName" },
+                values: new object[] { "john.smith@gmail.com", "John", "Smith" });
+
+            migrationBuilder.UpdateData(
+                table: "Contacts",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "Email", "FirstName", "LastName" },
+                values: new object[] { "john.smith@gmail.com", "John", "Smith" });
+        }
+    }
+}
