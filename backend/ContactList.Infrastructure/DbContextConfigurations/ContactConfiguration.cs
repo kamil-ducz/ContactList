b3c0aa84a971a1b7d17a9c19799cbd3083cb6608
Kamil D <kamilducz1989@gmail.com>
2023-08-06 10:27:44 +0200

    Hash passwords in database, use BCrypt nuget

--- backend/ContactList.Infrastructure/DbContextConfigurations/ContactConfiguration.cs
+++ backend/ContactList.Infrastructure/DbContextConfigurations/ContactConfiguration.cs
@@ -9,10 +9,10 @@ public class ContactConfiguration : IEntityTypeConfiguration<Contact>
     public void Configure(EntityTypeBuilder<Contact> builder)
     {
         builder.HasData(
-            new Contact { Id = 1, FirstName = "John", LastName = "Smith", Email = "john.smith@gmail.com", Password = "Ex@mpleP@$$sowrd", CategoryId = (int)Domain.Enums.ContactCategory.Private, SubCategoryId = null, PhoneNumber = "700-800-900", DateOfBirth = new DateTime(1999, 01, 01) },
-            new Contact { Id = 2, FirstName = "Jack", LastName = "Dean", Email = "jack.dean@gmail.com", Password = "Ex@mpleP@$$sowrd1", CategoryId = (int)Domain.Enums.ContactCategory.Work, SubCategoryId = (int)Domain.Enums.ContactSubCategory.Client, PhoneNumber = "777-888-999", DateOfBirth = new DateTime(1980, 03, 08) },
-            new Contact { Id = 3, FirstName = "Edgar", LastName = "Gold", Email = "eg@gmail.com", Password = "Ex@mpleP@$$sowrd2", CategoryId = (int)Domain.Enums.ContactCategory.Work, SubCategoryId = (int)Domain.Enums.ContactSubCategory.Boss, PhoneNumber = "500-600-700", DateOfBirth = new DateTime(1985, 03, 15) },
-            new Contact { Id = 4, FirstName = "Roger", LastName = "Sapphire", Email = "rsapphire@gmail.com", Password = "Ex@mpleP@$$sowrd3", CategoryId = (int)Domain.Enums.ContactCategory.Other, SubCategoryId = null, PhoneNumber = "555-666-777", DateOfBirth = new DateTime(1971, 06, 14) }
+            new Contact { Id = 1, FirstName = "John", LastName = "Smith", Email = "john.smith@gmail.com", Password = BCrypt.Net.BCrypt.HashPassword("Ex@mpleP@$$sowrd"), CategoryId = (int)Domain.Enums.ContactCategory.Private, SubCategoryId = null, PhoneNumber = "700-800-900", DateOfBirth = new DateTime(1999, 01, 01) },
+            new Contact { Id = 2, FirstName = "Jack", LastName = "Dean", Email = "jack.dean@gmail.com", Password = BCrypt.Net.BCrypt.HashPassword("Ex@mpleP@$$sowrd1"), CategoryId = (int)Domain.Enums.ContactCategory.Work, SubCategoryId = (int)Domain.Enums.ContactSubCategory.Client, PhoneNumber = "777-888-999", DateOfBirth = new DateTime(1980, 03, 08) },
+            new Contact { Id = 3, FirstName = "Edgar", LastName = "Gold", Email = "eg@gmail.com", Password = BCrypt.Net.BCrypt.HashPassword("Ex@mpleP@$$sowrd2"), CategoryId = (int)Domain.Enums.ContactCategory.Work, SubCategoryId = (int)Domain.Enums.ContactSubCategory.Boss, PhoneNumber = "500-600-700", DateOfBirth = new DateTime(1985, 03, 15) },
+            new Contact { Id = 4, FirstName = "Roger", LastName = "Sapphire", Email = "rsapphire@gmail.com", Password = BCrypt.Net.BCrypt.HashPassword("Ex@mpleP@$$sowrd3"), CategoryId = (int)Domain.Enums.ContactCategory.Other, SubCategoryId = null, PhoneNumber = "555-666-777", DateOfBirth = new DateTime(1971, 06, 14) }
             );
     }
 }
---
+++ backend/ContactList.Infrastructure/Migrations/20230806082453_Hash passwords for seeded data.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContactList.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Hashpasswordsforseededdata : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Contacts",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Password",
+                value: "$2a$11$st9HA1vEJWUbO5dKIcVZnO5c.6K2kDJJk04Czx7Br7RrottVW3z7u");
+
+            migrationBuilder.UpdateData(
+                table: "Contacts",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Password",
+                value: "$2a$11$3DPm5qcUBmjKY083tGBWzur3WSCMEThkohpmDz4h35FTT1cSX0cMq");
+
+            migrationBuilder.UpdateData(
+                table: "Contacts",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Password",
+                value: "$2a$11$5NRWhHQw5gWHSoh42AuGcexBHzfCTUJZAdsH83BgEHXlxbmaO/7Ui");
+
+            migrationBuilder.UpdateData(
+                table: "Contacts",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Password",
+                value: "$2a$11$cpDZ2XKcZRbKmKj9bfNAzuE0cYmiR73xleYF2iFzXdyvyesHhNu8e");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Contacts",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Password",
+                value: "Ex@mpleP@$$sowrd");
+
+            migrationBuilder.UpdateData(
+                table: "Contacts",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Password",
+                value: "Ex@mpleP@$$sowrd1");
+
+            migrationBuilder.UpdateData(
+                table: "Contacts",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Password",
+                value: "Ex@mpleP@$$sowrd2");
+
+            migrationBuilder.UpdateData(
+                table: "Contacts",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Password",
+                value: "Ex@mpleP@$$sowrd3");
+        }
+    }
+}
